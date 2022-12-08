7d319eebff202c438c70c3d52e04662f9cd7e0e3
HoangPhiem <tuhoangphiem10@gmail.com>
2022-12-08 13:00:50 +0700

    Authentication and authorization function

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,4 +1,10 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
+const AuthController = require('../../src/app/controllers/auth.controllers')
+const AuthMiddleware = require('../../src/app/middlewares/auth.middlewares')
+
+router.post('/register', AuthController.register)
+router.post('/login', AuthController.login)
+router.post('/refreshToken', AuthController.requestRefreshToken)
+router.post('/logout', AuthMiddleware.verifyToken, AuthController.logout)
 
 module.exports = router
