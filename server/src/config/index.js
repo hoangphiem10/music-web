8dc75100735b3e0fba97ba75909c19c3469998ca
HoangPhiem <tuhoangphiem10@gmail.com>
2022-12-10 12:00:03 +0700

    authentication and authorization API

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
-
+mongoose.set('strictQuery', false)
 async function connect() {
     try {
         await mongoose.connect(`mongodb://${process.env.DB}`)
 
-        console.log(process.env.DB, 'Connect successful!')
+        console.log(process.env.DB, 'Connect successfully!')
     } catch (err) {
         console.log('Connect failed!')
     }
