6561eb7cd3bafa5707be5b5057c3a69af0fe47c4
HoangPhiem <tuhoangphiem10@gmail.com>
2022-12-11 13:54:24 +0700

    SA-1 forgotPassword,resetPassword API and verify signup in server

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,10 +1,16 @@
 const router = require('express').Router()
 const AuthController = require('../../src/app/controllers/auth.controllers')
 const AuthMiddleware = require('../../src/app/middlewares/auth.middlewares')
-
-router.post('/register', AuthController.register)
+const verifySignUp = require('../../src/app/middlewares/verifySignUp')
+router.post(
+    '/register',
+    verifySignUp.checkDuplicateUsernameOrEmail,
+    AuthController.register,
+)
 router.post('/login', AuthController.login)
 router.post('/refreshToken', AuthController.requestRefreshToken)
 router.post('/logout', AuthMiddleware.verifyToken, AuthController.logout)
+router.post('/forgot-password', AuthController.forgotPassword)
+router.post('/reset-password/:id/:token', AuthController.resetPassword)
 
 module.exports = router
