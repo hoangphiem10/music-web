ddd2bfd60e93d67cd92d60d51e656b63cee25c57
HoangPhiem <tuhoangphiem10@gmail.com>
2023-01-31 23:32:17 +0700

    SA-3 verify token for contact api

--- server/src/routes/contact.js
+++ server/src/routes/contact.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const Contact = require('../../src/app/controllers/Contact.controllers')
+const AuthMiddleware = require('../../src/app/middlewares/auth.middlewares')
 
-router.post('/createContact', Contact.createContact)
+router.post('/createContact', AuthMiddleware.verifyToken, Contact.createContact)
 
 module.exports = router
